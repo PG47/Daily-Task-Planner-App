ba8ffbdbf23131b085508a2b13fa5dea2af99de2
PG47 <86549123+PG47@users.noreply.github.com>
2023-09-10 17:02:32 +0700

    complete adding homescreen and add task box

--- source/daily_task_plan/lib/screen/home.dart
+++ source/daily_task_plan/lib/screen/home.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+//import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -10,8 +10,8 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          'Tasks Manager',
-          style: GoogleFonts.robotoFlex(),
+          'Task Manager',
+          //style: GoogleFonts.montserrat(),
         ),
       ),
       body: Center(
@@ -26,12 +26,68 @@ class HomeScreen extends StatelessWidget {
         onPressed: () => showModalBottomSheet(
             context: context,
             builder: (BuildContext context) => Container(
-                  //height: 500,
+                  padding: const EdgeInsets.all(10.0),
+                  height: 200,
                   color: Colors.blue[200],
                   child: Column(
                     children: [
-                      Text(
-                        'ADD TASK',
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'ADD TASK',
+                            /*style: GoogleFonts.montserrat(
+                              color: Colors.white,
+                              fontSize: 20.0,
+                            ),*/
+                          ),
+                          GestureDetector(
+                              onTap: () => Navigator.of(context).pop(),
+                              child: Icon(
+                                Icons.close,
+                              ))
+                        ],
+                      ),
+                      Divider(
+                        height: 1.5,
+                        thickness: 1.5,
+                      ),
+                      SizedBox(
+                        height: 20.5,
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(6.0),
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                          fillColor: Colors.white,
+                          filled: true,
+                          hintText: 'Task Name',
+                        ),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.symmetric(horizontal: 20),
+                        width: MediaQuery.of(context).size.width / 2,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            ElevatedButton(
+                              child: Text('Reset'),
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.black,
+                              ),
+                              onPressed: () => print('reset pressed'),
+                            ),
+                            ElevatedButton(
+                              child: Text('Add'),
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.black,
+                              ),
+                              onPressed: () => print('add pressed'),
+                            ),
+                          ],
+                        ),
                       ),
                     ],
                   ),
